6daba01a0e6e6c23f213ec2977ff989ba99a15fe
shihyuuuuuuu <www.elmo20816@gmail.com>
2019-01-07 20:23:01 +0800

    Add minimax function, but still stupid AI.(random evaluation yet)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var board = [];
 var nowPlayer;
 var opponent;
-var legalPlace = [];
+var bestChild;
+//var legalPlace = [];
 
 initial();
 update_board();
@@ -11,13 +12,13 @@ function initial() {
     for (var r = 0; r < 8; r++) {
         board[r] = [];
         for (var c = 0; c < 8; c++) {
-            board[r][c] = { val: -1 };
+            board[r][c] = -1;
         }
     }
-    board[3][3].val = 1;
-    board[3][4].val = 0;
-    board[4][3].val = 0;
-    board[4][4].val = 1;
+    board[3][3] = 1;
+    board[3][4] = 0;
+    board[4][3] = 0;
+    board[4][4] = 1;
 }
 
 function firstPlayer(player) {
@@ -31,44 +32,45 @@ function firstPlayer(player) {
     document.getElementsByClassName("btn")[0].disabled = true;
     document.getElementsByClassName("btn")[1].disabled = true;
 }
-
+var temp;
 function nextPlayer() {
     opponent = Math.abs(nowPlayer - 1);
-    legalMove();
-    console.log(legalPlace);
+    var map = $.extend(true, {}, board);
+    var legalPlace = legalMove(map);
     if (nowPlayer == 0) {
-        hint();
+        hint(legalPlace);
     } else
-        search();
+        search(legalPlace, map);
 }
 
-function legalMove() {
+function legalMove(map) {
+    var legalPlace = [];
     for (var r = 0; r < 8; r++) {
         for (var c = 0; c < 8; c++) {
-            if (board[r][c].val == -1) {
+            if (map[r][c] == -1) {
                 var thisPlaceAvailable = false;
-                var vertical = getVerticalLine(c);
+                var vertical = getVerticalLine(c, map);
                 thisPlaceLegal = check(vertical, r, opponent, false);
                 if (thisPlaceLegal == true) {
                     console.log("vertical", r, c);
                     legalPlace.push([r, c]);
                     continue;
                 }
-                var horizontal = getHorizontalLine(r);
+                var horizontal = getHorizontalLine(r, map);
                 thisPlaceLegal = check(horizontal, c, opponent, false);
                 if (thisPlaceLegal == true) {
                     console.log("horizontal", r, c);
                     legalPlace.push([r, c]);
                     continue;
                 }
-                var leftSlash = getLeftSlash(r, c);
+                var leftSlash = getLeftSlash(r, c, map);
                 thisPlaceLegal = check(leftSlash, (r <= c) ? r : c, opponent, false);
                 if (thisPlaceLegal == true) {
                     console.log("left", r, c);
                     legalPlace.push([r, c]);
                     continue;
                 }
-                var rightSlash = getRightSlash(r, c);
+                var rightSlash = getRightSlash(r, c, map);
                 thisPlaceLegal = check(rightSlash, (r + c <= 7) ? r : 7 - c, opponent, false);
                 if (thisPlaceLegal == true) {
                     console.log("right", r, c);
@@ -77,25 +79,26 @@ function legalMove() {
             }
         }
     }
+    return legalPlace;
 }
 
-function getVerticalLine(c) { // 取得第c排所有棋子
+function getVerticalLine(c, map) { // 取得第c排所有棋子
     var arr = [];
     for (var r = 0; r < 8; ++r) {
-        arr.push({ val: board[r][c].val, r: r, c: c });
+        arr.push({ val: map[r][c], r: r, c: c });
     }
     return arr;
 }
 
-function getHorizontalLine(r) { // 取得第r列所有棋子
+function getHorizontalLine(r, map) { // 取得第r列所有棋子
     var arr = [];
     for (var c = 0; c < 8; ++c) {
-        arr.push({ val: board[r][c].val, r: r, c: c });
+        arr.push({ val: map[r][c], r: r, c: c });
     }
     return arr;
 }
 
-function getLeftSlash(r, c) { // 取得第(r, c)位置的左斜線所有棋子
+function getLeftSlash(r, c, map) { // 取得第(r, c)位置的左斜線所有棋子
     var arr = [];
     if (r >= c) {
         r = r - c; c = 0;
@@ -103,13 +106,13 @@ function getLeftSlash(r, c) { // 取得第(r, c)位置的左斜線所有棋子
         c = c - r; r = 0;
     }
     while (r <= 7 && c <= 7) {
-        arr.push({ val: board[r][c].val, r: r, c: c });
+        arr.push({ val: map[r][c], r: r, c: c });
         r++; c++;
     }
     return arr;
 }
 
-function getRightSlash(r, c) { // 取得第(r, c)位置的右斜線所有棋子
+function getRightSlash(r, c, map) { // 取得第(r, c)位置的右斜線所有棋子
     var arr = [];
     if (r + c <= 7) {
         c = r + c; r = 0;
@@ -117,7 +120,7 @@ function getRightSlash(r, c) { // 取得第(r, c)位置的右斜線所有棋子
         r = r + c - 7; c = 7;
     }
     while (r <= 7 && c >= 0) {
-        arr.push({ val: board[r][c].val, r: r, c: c });
+        arr.push({ val: map[r][c], r: r, c: c });
         r++; c--;
     }
     return arr;
@@ -140,7 +143,7 @@ function check_up(array, index, opponent, flip) {
                     if (array[index].val == nowPlayer) {
                         if (flip == true) {
                             while (index <= temp_index) {
-                                board[array[index].r][array[index].c].val = nowPlayer;
+                                board[array[index].r][array[index].c] = nowPlayer;
                                 index++;
                             }
                         }
@@ -166,7 +169,7 @@ function check_down(array, index, opponent, flip) {
                     if (array[index].val == nowPlayer) {
                         if (flip == true) {
                             while (index >= temp_index) {
-                                board[array[index].r][array[index].c].val = nowPlayer;
+                                board[array[index].r][array[index].c] = nowPlayer;
                                 index--;
                             }
                         }
@@ -181,7 +184,7 @@ function check_down(array, index, opponent, flip) {
     return false;
 }
 
-function hint() {
+function hint(legalPlace) {
     for (var i = 0; i < legalPlace.length; ++i) {
         document.querySelector(".r" + legalPlace[i][0] + " .c" + legalPlace[i][1]).disabled = false;
         document.querySelector(".r" + legalPlace[i][0] + " .c" + legalPlace[i][1] + " .dot").style.background = "white";
@@ -195,45 +198,102 @@ function update_board() {
             document.querySelector(".r" + r + " .c" + c).disabled = true;
             var dot = document.querySelector(".r" + r + " .c" + c + " .dot");
             dot.style.opacity = "1";
-            if (board[r][c].val == 1) {
+            if (board[r][c] == 1) {
                 dot.style.background = "white";
-            } else if (board[r][c].val == 0) {
+            } else if (board[r][c] == 0) {
                 dot.style.background = "black";
-            } else if (board[r][c].val == -1) {
+            } else if (board[r][c] == -1) {
                 dot.style.background = "";
             }
         }
     }
-    printBoard();
+    printBoard(board);
 }
 
 function flip(r, c) {
-    var vertical = getVerticalLine(c);
+    var vertical = getVerticalLine(c, board);
     check(vertical, r, opponent, true);
-    var horizontal = getHorizontalLine(r);
+    var horizontal = getHorizontalLine(r, board);
     check(horizontal, c, opponent, true);
-    var leftSlash = getLeftSlash(r, c);
+    var leftSlash = getLeftSlash(r, c, board);
     check(leftSlash, (r <= c) ? r : c, opponent, true);
-    var rightSlash = getRightSlash(r, c);
+    var rightSlash = getRightSlash(r, c, board);
     check(rightSlash, (r + c <= 7) ? r : 7 - c, opponent, true);
 }
 
 function put(r, c) {
-    board[r][c].val = nowPlayer;
+    board[r][c] = nowPlayer;
     flip(r, c);
     update_board();
-    legalPlace = [];
+    //legalPlace = [];
     nowPlayer = Math.abs(nowPlayer - 1);
     nextPlayer();
 }
 
-function search() {
+function search(temp, map) {
     console.log("Computer searching start...");
-    put(legalPlace[0][0], legalPlace[0][1]);
+    //put(temp[0][0], temp[0][1]);
+    var legalPlace = legalMove(map);
+    var score = minimax(map, 3, -10000, 10000, true);
+    console.log(score);
+    console.log(bestChild);
+    put(bestChild[0], bestChild[1]);
 }
 
-function printBoard() {
+function minimax(map, depth, alpha, beta, maximizingPlayer){
+    if(depth == 0){
+        return evaluation_function(map);
+    }
+
+    if(maximizingPlayer){
+        var maxEval = -10000;
+        var next = legalMove(map);
+        for(var i = 0; i < next.length; i++){
+            var temp_map = $.extend(true, {}, map);
+            temp_map[next[i][0]][next[i][1]] = 1;
+            /*console.log("temp::");
+            printBoard(temp_map);
+            console.log(next[i][0], next[i][1]);*/
+            var eval = minimax(temp_map, depth-1, alpha, beta, false);
+            console.log("evalmax:", eval, i);
+            if(maxEval < eval){
+                maxEval = eval;
+                bestChild = [next[i][0], next[i][1]];
+            }
+            alpha = Math.max(alpha, eval);
+            if(beta <= alpha)
+                break;
+        }
+        return maxEval;
+    }else{
+        var minEval = 10000;
+        var next = legalMove(map);
+        for(var i = 0; i < next.length; i++){
+            var temp_map = $.extend(true, {}, map);
+            temp_map[next[i][0]][next[i][1]] = -1;
+            /*console.log("temp::");
+            printBoard(temp_map);
+            console.log(next[i][0], next[i][1]);*/
+            var eval = minimax(temp_map, depth-1, alpha, beta, true);
+            console.log("evalmin:", eval, i);
+            if(minEval > eval){
+                minEval = eval;
+                bestChild = [next[i][0], next[i][1]];
+            }
+            beta = Math.min(beta, eval);
+            if(beta <= alpha)
+                break;
+        }
+        return minEval;
+    }
+}
+
+function evaluation_function(map){
+    return Math.random()*100 + 1;
+}
+
+function printBoard(board) {
     for (var r = 0; r < 8; r++) {
-        console.log(board[r][0].val, board[r][1].val, board[r][2].val, board[r][3].val, board[r][4].val, board[r][5].val, board[r][6].val, board[r][7].val);
+        console.log(board[r][0], board[r][1], board[r][2], board[r][3], board[r][4], board[r][5], board[r][6], board[r][7]);
     }
 }
